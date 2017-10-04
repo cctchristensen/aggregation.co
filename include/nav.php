33c38995dfbff65301f907e2245b25fff6e335c0
Cole Christensen <cctchristensen@gmail.com>
2017-10-03 21:51:44 -0700

    created new feature page

--- include/nav.php
+++ include/nav.php
@@ -1,5 +1,6 @@
 <nav>
 <span><a href="index.php">News</a></span>
 <span><a href="feeds.php">Feeds</a></span>
+<span><a href="newfeature.php">Add Feeds</a></span>
 <span><a href="help.php">Help</a></span>
 </nav>
---
+++ newfeature.php
@@ -0,0 +1,14 @@
+<?php
+require("include/header.php");
+require("include/nav.php");
+?>
+
+<div class="help">This is a new feature added to aggregation.co by Cole Christensen.</div>
+
+<div class="help">Simply copy/paste the url for the RSS feed you desire and it will be automatically entered in the database.</div>
+
+
+
+<?php
+require("include/footer.php");
+?>
